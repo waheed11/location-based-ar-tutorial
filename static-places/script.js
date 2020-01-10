b6e05ceee7d1ae8d8e559660293ae8db82d5dbbd
waheed11 <abzaydi@gmail.com>
2020-01-10 06:59:42 +0300

    change the sign to text

--- static-places/script.js
+++ static-places/script.js
@@ -1,7 +1,7 @@
 
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
+    button.innerText = 'RECORD';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
